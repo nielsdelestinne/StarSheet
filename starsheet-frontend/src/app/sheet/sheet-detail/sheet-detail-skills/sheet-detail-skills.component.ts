2e4c811201c2b450695358b8fefb40943d6fecf9
nielsdelestinne <nielsdelestinne@gmail.com>
2018-08-20 20:39:37 +0200

    skills basic implementation finished (seperate component for each skill in the skills overview + 2-way-binding)

--- starsheet-frontend/src/app/sheet/sheet-detail/sheet-detail-skills/sheet-detail-skills.component.ts
+++ starsheet-frontend/src/app/sheet/sheet-detail/sheet-detail-skills/sheet-detail-skills.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SheetDetailSkillsComponent implements OnInit {
 
+  protected skills : any;
+
   constructor() { }
 
   ngOnInit() {
+    this.initializeSkills();
+  }
+
+  private initializeSkills() : void {
+    this.skills = {
+      acrobatics: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      athletics: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      bluff: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      computers: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      culture: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      diplomacy: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      disguise: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      engineering: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      intimidate: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      lifeScience: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      medicine: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      mysticism: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      perception: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      physicalScience: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      piloting: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      profession: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      senseMotive: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      sleightOfHand: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      stealth: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+      survival: {total: 0, ranks: 0, classBonus: 0, abilityMod: 0, miscMod: 0},
+    };
   }
 
 }
--- starsheet-frontend/src/app/sheet/sheet-detail/sheet-detail-skills/skill-line/skill-line.component.ts
+++ starsheet-frontend/src/app/sheet/sheet-detail/sheet-detail-skills/skill-line/skill-line.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-skill-line',
+  selector: '[app-skill-line]',
   templateUrl: './skill-line.component.html',
   styleUrls: ['./skill-line.component.css']
 })
 export class SkillLineComponent implements OnInit {
 
-  constructor() { }
+  @Input() skillType: string;
+  @Input() skillsForType: any;
+  @Output() skillsForTypeChange = new EventEmitter<any>();
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  onRanksChange() {
+    this.onChangeOfSkill();
+  }
+
+  onClassBonusChange() {
+    this.onChangeOfSkill();
+  }
+
+  onMiscModChange() {
+    this.onChangeOfSkill();
+  }
+
+  private onChangeOfSkill() {
+    this.recalculateTotal();
+    this.skillsForTypeChange.emit(this.skillsForType);
+  }
+
+  private recalculateTotal() {
+    this.skillsForType.total = this.skillsForType.ranks
+      + this.skillsForType.classBonus
+      + this.skillsForType.abilityMod
+      + this.skillsForType.miscMod;
   }
 
 }
